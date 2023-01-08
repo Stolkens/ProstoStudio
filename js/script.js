518342ef61b87caf6021b3bad1d30bf7dbcacd58
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-01-08 07:50:52 +0100

    Add generated project list

--- js/script.js
+++ js/script.js
@@ -1,47 +1,84 @@
-'use strict';
+{
+  'use strict';
 
-const listClickHandler = function (event) {
-    event.preventDefault ();
+  const select = {
+    all: {
+      project: '.project',
+      contact: '.contact',
+      projectTitle: '.project-title',
+    },
+    listOf: {
+      projects: '.projects.list',
+      contact: '.list.contacts' 
+    }
+  };
+ 
+
+
+
+
+  const listClickHandler = function (event) {
+    event.preventDefault();
     const clickedLink = this;
-    console.log("link was clicked");
-
     /* remove class'active' from all links */
     const projectLinks = document.querySelectorAll('.list a.active');
-    for(let projectlink of projectLinks){
-        projectlink.classList.remove('active');
+    for (const projectlink of projectLinks) {
+      projectlink.classList.remove('active');
     }
-
     /* add class 'active' to clicked link */
     clickedLink.classList.add('active');
 
-    /* remove class 'active' from all projects and contacts*/
-    const projects = document.querySelectorAll('.project');
-    for(let project of projects){
-        project.classList.remove('active');
+    /* remove class 'active' from all projects and contacts */
+    const projects = document.querySelectorAll(select.all.project);
+    for (const project of projects) {
+      project.classList.remove('active');
     }
-    const contacts = document.querySelectorAll('.contact');
-    for(let contact of contacts){
-        contact.classList.remove('active');
-    } 
-
+    const contacts = document.querySelectorAll(select.all.contact);
+    for (const contact of contacts) {
+      contact.classList.remove('active');
+    }
     /* get href attribute from cklicked link */
     const projectAttribute = clickedLink.getAttribute('href');
-    
 
     /* find project with href attribute */
     const targetProject = document.querySelector(projectAttribute);
-    
 
     /* add to project class 'active */
-    targetProject.classList.add('active')
+    targetProject.classList.add('active');
+  };
+  
 
-    
-}
-const links = document.querySelectorAll('.list a');
-    
-    for(let link of links){
-        link.addEventListener('click', listClickHandler);
-        
+  const generateProjectList = function (){
+    const projectList = document.querySelector(select.listOf.projects);
+    console.log('projectlist', projectList);
+    projectList.innerHTML = '';
+    console.log('projectList', projectList.innerHTML);
+    const projects = document.querySelectorAll(select.all.project);
+    console.log('projects', projects);
+    let titleList = '';
+    for(let project of projects){
+      const projectID = project.getAttribute('id');
+      console.log('projectID', projectID);
+      const projectTitleElement = project.querySelector('.project-title');
+      console.log('projectTitle', projectTitleElement);
+      const projectTitle = projectTitleElement.innerHTML;
+      const projectLinkHtml = '<li><a href = "#' + projectID + '">' + projectTitle + '</a></li>';
+      console.log('projectlinkhtml', projectLinkHtml);
+      titleList = titleList + projectLinkHtml;
+      
+    }
+    projectList.innerHTML = titleList;
+    console.log('titlelist', titleList);
+    const links = document.querySelectorAll('.list a');
+
+    for (const link of links) {
+      link.addEventListener('click', listClickHandler);
     }
 
-    
+  };
+
+  generateProjectList ();
+
+
+
+}
